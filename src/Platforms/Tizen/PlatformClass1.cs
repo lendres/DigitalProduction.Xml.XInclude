7c76f2679fc738078be626e97e1469bffaa0c06b
Ikosy <lendres@fifthrace.com>
2024-03-09 12:38:06 -0600

    Initial check in

---
+++ src/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace XInclude_Maui
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
---
+++ src/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace XInclude_Maui
+{
+	// All the code in this file is only included on iOS.
+	public class PlatformClass1
+	{
+	}
+}
