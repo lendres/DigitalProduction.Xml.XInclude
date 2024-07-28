88787cc3a3a25f3b9e95be0ab0cf1ce303644553
Ikosy <lendres@fifthrace.com>
2024-07-28 07:08:21 -0500

    Minor updates to version and authors

--- src/publish-nuget.bat
+++ src/publish-nuget.bat
@@ -5,9 +5,7 @@ set /p api-key=<"../../Keys/Nuget/api-key.txt
 echo API KEY: %api-key%
 
 rem Change to the package directory.
-echo %cd%
 chdir /d ./bin/Release
-
 echo %cd%
 
 rem Publish.
@@ -16,9 +14,5 @@ for /f %%a in ('call dir *.nupkg /b') do (
 	echo Processing: %%a
 	dotnet nuget push %%a --api-key %api-key% --source https://api.nuget.org/v3/index.json --skip-duplicate
 )
-
-
-
-rem dotnet nuget push DigitalProductionDotNetLibrary.0.1.1.nupkg --api-key %api-key% --source https://api.nuget.org/v3/index.json --skip-duplicate
 
 pause
