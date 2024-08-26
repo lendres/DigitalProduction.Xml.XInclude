eee022ba98ce39e96615133e79ad7adae138fa3b
Ikosy <lendres@fifthrace.com>
2024-08-26 06:27:25 -0500

    Organize publishing

--- src/Publishing/publish-nuget.bat
+++ src/Publishing/publish-nuget.bat
@@ -1,11 +1,11 @@
 @echo off
 
 rem Get the api key.
-set /p api-key=<"../../Keys/Nuget/api-key.txt
+set /p api-key=<"../../../Keys/Nuget/api-key.txt
 echo API KEY: %api-key%
 
 rem Change to the package directory.
-chdir /d ./bin/Release
+chdir /d ../bin/Release
 echo %cd%
 
 rem Publish.
