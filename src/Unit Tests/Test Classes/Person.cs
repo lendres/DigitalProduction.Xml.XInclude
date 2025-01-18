fcde7fb0a4accdde7e207a3114c435cc86e64a93
Ikosy <lendres@fifthrace.com>
2025-01-18 07:32:53 -0600

    Add unit tests

---
+++ src/Unit Tests/Test Classes/Person.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Serialization;
+
+namespace DigitalProduction.UnitTests;
+
+/// <summary>
+/// A person.
+/// </summary>
+public class Person
+{
+	#region Construction
+
+	/// <summary>
+	/// Default constructor.  Required for serialization.
+	/// </summary>
+	public Person()
+	{
+	}
+
+	/// <summary>
+	/// Constructor to populate fields.
+	/// </summary>
+	public Person(string name, int age, Gender gender)
+	{
+		Name	= name;
+		Age		= age;
+		Gender	= gender;
+	}
+
+	#endregion
+
+	#region Properties
+
+	/// <summary>
+	/// Name.
+	/// </summary>
+	[XmlAttribute("name")]
+	public string Name { get; set; } = "";
+
+	/// <summary>
+	/// Age.
+	/// </summary>
+	[XmlAttribute("age")]
+	public int Age { get; set; } = 0;
+
+	/// <summary>
+	/// Gender.
+	/// </summary>
+	[XmlAttribute("gender")]
+	public Gender Gender  { get; set; } = Gender.Female;
+
+	#endregion
+
+} // End class.
